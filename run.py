840bed129b7c6539ac0be90a752abc7704e3e1f8
wtbates99 <wtbates99@gmail.com>
2024-03-16 00:01:20 -0400

    updated stock_data to align df to model input and model data

--- run.py
+++ run.py
@@ -2,8 +2,8 @@ from stock_data import pull_stocks
 from model_create import create_xgb_model
 
 if __name__ == "__main__":
-    stocks = ["MSFT", "AAPL", "TSLA"]  # Example list of pull_stocks
-
+    # stocks = ["MSFT", "AAPL", "TSLA"]  # Example list of pull_stocks
+    stocks = ["KO"]
     model_data, input = pull_stocks(stocks)
     model_data.to_csv("model_data.csv")
     print(model_data.columns)
--- stock_data.py
+++ stock_data.py
@@ -6,6 +6,7 @@ from metrics import calculate_metrics
 def pull_single_stock(ticker):
     stock = yf.Ticker(ticker)
     hist = stock.history(period="max", interval="1d")
+    hist["Prev_Day_Close"] = hist["Close"].shift(1)
     hist.reset_index(inplace=True)
     hist = calculate_metrics(hist)
     hist.dropna(inplace=True)
@@ -20,6 +21,8 @@ def pull_stocks(tickers):
     for tick_x in tickers:
         stock_data = pull_single_stock(tick_x)
         input_value = stock_data.iloc[-1:]
+        input_value["Prev_Day_Close"] = input_value["Close"]
+        input_value = input_value.drop(columns=["Date", "Close", "Ticker"])
         all_stocks.append(stock_data)
 
     # Concatenate all stock_data into a single stock_dataFrame
