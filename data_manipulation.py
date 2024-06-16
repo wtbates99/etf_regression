65e707bfe46a5c1db421b1ad9cc7d4e52d43cf36
wtbates99 <wtbates99@gmail.com>
2024-06-16 18:43:00 -0400

    need to fix refactoring

--- data_manipulation.py
+++ data_manipulation.py
@@ -1,121 +1,118 @@
-import os
 import sqlite3
 import pandas as pd
 import numpy as np
 import ta
 
-# Database path
-db_path = os.path.expanduser("~/personal_git/stock_price_predictor/db/stock_data.db")
+conn = sqlite3.connect("_stock_data.db")
 
-# Connect to the SQLite database
-conn = sqlite3.connect(db_path)
+t_query = """
+SELECT Date, Ticker,
+       SUM(Open) as Open,
+       SUM(Close) as Close,
+       SUM(High) as High,
+       SUM(Low) as Low,
+       SUM(Volume) as Volume
+FROM historicals_with_sector
+GROUP BY Ticker, Date
+ORDER BY Ticker, Date
+"""
 
-# Read stock_data table into a DataFrame
-df = pd.read_sql_query(
-    "SELECT Date, Ticker, Open, Close, High, Low, Volume, Sector, Subsector FROM historicals_with_sector",
-    conn,
-)
+s_query = """
+SELECT Date, Sector,
+       SUM(Open) as Open,
+       SUM(Close) as Close,
+       SUM(High) as High,
+       SUM(Low) as Low,
+       SUM(Volume) as Volume
+FROM historicals_with_sector
+GROUP BY Sector, Date
+ORDER BY Sector, Date
+"""
 
-# Ensure Date column is datetime
-df["Date"] = pd.to_datetime(df["Date"])
+ss_query = """
+SELECT Subsector, Date,
+       SUM(Open) as Open,
+       SUM(Close) as Close,
+       SUM(High) as High,
+       SUM(Low) as Low,
+       SUM(Volume) as Volume
+FROM historicals_with_sector
+GROUP BY Subsector, Date
+ORDER BY Subsector, Date
+"""
 
 
-# Function to calculate technical indicators with a prefix
-def calculate_indicators(group, prefix):
+def calculate_indicators(df: pd.DataFrame, prefix: str) -> pd.DataFrame:
+    close = df["Close"]
+    high = df["High"]
+    low = df["Low"]
+    volume = df["Volume"]
+
     indicators = {
-        f"{prefix}_SMA_10": ta.trend.sma_indicator(group["Close"], window=10),
-        f"{prefix}_SMA_50": ta.trend.sma_indicator(group["Close"], window=50),
-        f"{prefix}_EMA_10": ta.trend.ema_indicator(group["Close"], window=10),
-        f"{prefix}_EMA_50": ta.trend.ema_indicator(group["Close"], window=50),
-        f"{prefix}_RSI": ta.momentum.rsi(group["Close"], window=14),
+        f"{prefix}": prefix,
+        f"{prefix}_SMA_10": ta.trend.sma_indicator(close, window=10),
+        f"{prefix}_EMA_10": ta.trend.ema_indicator(close, window=10),
+        f"{prefix}_RSI": ta.momentum.rsi(close, window=14),
         f"{prefix}_Stochastic_K": ta.momentum.stoch(
-            group["High"], group["Low"], group["Close"], window=14, smooth_window=3
+            high, low, close, window=14, smooth_window=3
         ),
         f"{prefix}_Stochastic_D": ta.momentum.stoch_signal(
-            group["High"], group["Low"], group["Close"], window=14, smooth_window=3
+            high, low, close, window=14, smooth_window=3
         ),
-        f"{prefix}_MACD": ta.trend.macd(group["Close"]),
-        f"{prefix}_MACD_Signal": ta.trend.macd_signal(group["Close"]),
-        f"{prefix}_MACD_Diff": ta.trend.macd_diff(group["Close"]),
-        f"{prefix}_TSI": ta.momentum.tsi(group["Close"]),
-        f"{prefix}_UO": ta.momentum.ultimate_oscillator(
-            group["High"], group["Low"], group["Close"]
-        ),
-        f"{prefix}_ROC": ta.momentum.roc(group["Close"], window=12),
-        f"{prefix}_Williams_R": ta.momentum.williams_r(
-            group["High"], group["Low"], group["Close"], lbp=14
-        ),
-        f"{prefix}_ATR": ta.volatility.average_true_range(
-            group["High"], group["Low"], group["Close"], window=14
-        ),
+        f"{prefix}_MACD": ta.trend.macd(close),
+        f"{prefix}_MACD_Signal": ta.trend.macd_signal(close),
+        f"{prefix}_MACD_Diff": ta.trend.macd_diff(close),
+        f"{prefix}_TSI": ta.momentum.tsi(close),
+        f"{prefix}_UO": ta.momentum.ultimate_oscillator(high, low, close),
+        f"{prefix}_ROC": ta.momentum.roc(close, window=12),
+        f"{prefix}_Williams_R": ta.momentum.williams_r(high, low, close, lbp=14),
         f"{prefix}_Bollinger_High": ta.volatility.bollinger_hband(
-            group["Close"], window=20, window_dev=2
+            close, window=20, window_dev=2
         ),
         f"{prefix}_Bollinger_Low": ta.volatility.bollinger_lband(
-            group["Close"], window=20, window_dev=2
+            close, window=20, window_dev=2
         ),
-        f"{prefix}_Bollinger_Mid": ta.volatility.bollinger_mavg(
-            group["Close"], window=20
-        ),
+        f"{prefix}_Bollinger_Mid": ta.volatility.bollinger_mavg(close, window=20),
         f"{prefix}_Bollinger_PBand": ta.volatility.bollinger_pband(
-            group["Close"], window=20, window_dev=2
+            close, window=20, window_dev=2
         ),
         f"{prefix}_Bollinger_WBand": ta.volatility.bollinger_wband(
-            group["Close"], window=20, window_dev=2
+            close, window=20, window_dev=2
         ),
-        f"{prefix}_On_Balance_Volume": ta.volume.on_balance_volume(
-            group["Close"], group["Volume"]
-        ),
+        f"{prefix}_On_Balance_Volume": ta.volume.on_balance_volume(close, volume),
         f"{prefix}_Chaikin_MF": ta.volume.chaikin_money_flow(
-            group["High"], group["Low"], group["Close"], group["Volume"], window=20
+            high, low, close, volume, window=20
         ),
-        f"{prefix}_Force_Index": ta.volume.force_index(
-            group["Close"], group["Volume"], window=13
-        ),
+        f"{prefix}_Force_Index": ta.volume.force_index(close, volume, window=13),
         f"{prefix}_MFI": ta.volume.money_flow_index(
-            group["High"], group["Low"], group["Close"], group["Volume"], window=14
+            high, low, close, volume, window=14
         ),
-        f"{prefix}_ADX": ta.trend.adx(
-            group["High"], group["Low"], group["Close"], window=14
-        ),
-        f"{prefix}_CCI": ta.trend.cci(
-            group["High"], group["Low"], group["Close"], window=20
-        ),
-        f"{prefix}_DPO": ta.trend.dpo(group["Close"], window=20),
-        f"{prefix}_KST": ta.trend.kst(group["Close"]),
-        f"{prefix}_KST_Signal": ta.trend.kst_sig(group["Close"]),
     }
 
     indicators_df = pd.DataFrame(indicators)
+    indicators_df = indicators_df.replace([np.inf, -np.inf], np.nan).ffill()
 
-    # Replace infinite values with NaNs
-    indicators_df = indicators_df.replace([np.inf, -np.inf], np.nan)
-    indicators_df = indicators_df.ffill()
-
-    return pd.concat([group, indicators_df], axis=1)
+    return pd.concat([prefix, indicators_df], axis=1)
 
 
-# Calculate indicators for different groupings and add prefixes
-df = (
-    df.sort_values(["Ticker", "Date"])
-    .groupby("Ticker")
-    .apply(calculate_indicators, "Ticker")
-    .reset_index(drop=True)
-)
-df = (
-    df.sort_values(["Sector", "Date"])
-    .groupby("Sector")
-    .apply(calculate_indicators, "Sector")
-    .reset_index(drop=True)
-)
-df = (
-    df.sort_values(["Subsector", "Date"])
-    .groupby("Subsector")
-    .apply(calculate_indicators, "Subsector")
-    .reset_index(drop=True)
-)
+t_df = pd.read_sql_query(t_query, conn)
+s_df = pd.read_sql_query(s_query, conn)
+ss_df = pd.read_sql_query(ss_query, conn)
 
-# Ensure the DataFrame is loaded to the SQLite database
-df.to_sql("stock_data_with_indicators", conn, if_exists="replace", index=False)
+
+t_df["Date"] = pd.to_datetime(t_df["Date"])
+s_df["Date"] = pd.to_datetime(s_df["Date"])
+ss_df["Date"] = pd.to_datetime(ss_df["Date"])
+
+t_df = calculate_indicators(t_df, "Ticker")
+print("t")
+s_df = calculate_indicators(s_df, "Sector")
+print("s")
+ss_df = calculate_indicators(ss_df, "Subsector")
+print("ss")
+
+t_df.to_sql("stock_data_with_indicators", conn, if_exists="replace", index=False)
+s_df.to_sql("stock_data_with_indicators", conn, if_exists="replace", index=False)
+ss_df.to_sql("stock_data_with_indicators", conn, if_exists="replace", index=False)
 
 conn.close()
--- data_pull.py
+++ data_pull.py
@@ -1,228 +1,85 @@
-import yfinance as yf
 import pandas as pd
 import sqlite3
-import os
-from tqdm import tqdm
+import yfinance as yf
 
 
-def get_sp500_tickers():
-    sp500_url = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
-    table = pd.read_html(sp500_url)[0]
-    filtered_table = table[~table["Symbol"].str.contains(r"\.")]
-    return filtered_table["Symbol"].tolist()
+def fetch_sp500_tickers(
+    url="https://en.wikipedia.org/wiki/List_of_S%26P_500_companies",
+) -> list:
+    table = pd.read_html(url)[0]
+    tickers = table[~table["Symbol"].str.contains(r"\.")]["Symbol"].tolist()
+    return tickers
 
 
-# Function to download data in batches
-def download_stock_data(tickers, batch_size):
+def fetch_quant_data(tickers, batch_size=503) -> pd.DataFrame:
     all_data = []
     for i in range(0, len(tickers), batch_size):
         batch = tickers[i : i + batch_size]
         data = yf.download(batch, interval="1d", group_by="ticker", auto_adjust=True)
         all_data.append(data)
-    return all_data
-
-
-# Function to store stock price data in SQLite
-def store_stock_data_in_sqlite(data_list, db_path):
-    conn = sqlite3.connect(os.path.expanduser(db_path))
-    concatenated_data = pd.concat(data_list)
+    concatenated_data = pd.concat(all_data)
     flat_data = (
         concatenated_data.stack(level=0)
         .reset_index()
         .rename(columns={"level_1": "Ticker"})
     )
-    flat_data.to_sql("stock_data", conn, if_exists="replace", index=False)
-    conn.close()
+    return flat_data
 
 
-# Function to get comprehensive stock information in batches
-def get_stock_information_in_batches(tickers, batch_size):
+def fetch_qual_data(tickers, batch_size=503) -> pd.DataFrame:
     stock_information = []
     for i in range(0, len(tickers), batch_size):
         batch = tickers[i : i + batch_size]
-        for ticker in tqdm(batch, desc="Fetching categorical stock information..."):
-            try:
-                stock_info = yf.Ticker(ticker).info
-                stock_data = {
-                    "Ticker": ticker,
-                    "Sector": stock_info.get("sector", "N/A"),
-                    "Subsector": stock_info.get("industry", "N/A"),
-                    "FullName": stock_info.get("longName", "N/A"),
-                    "MarketCap": stock_info.get("marketCap", "N/A"),
-                    "Country": stock_info.get("country", "N/A"),
-                    "Website": stock_info.get("website", "N/A"),
-                    "Description": stock_info.get("longBusinessSummary", "N/A"),
-                    "CEO": stock_info.get("ceo", "N/A"),
-                    "Employees": stock_info.get("fullTimeEmployees", "N/A"),
-                    "City": stock_info.get("city", "N/A"),
-                    "State": stock_info.get("state", "N/A"),
-                    "Zip": stock_info.get("zip", "N/A"),
-                    "Address": stock_info.get("address1", "N/A"),
-                    "Phone": stock_info.get("phone", "N/A"),
-                    "Exchange": stock_info.get("exchange", "N/A"),
-                    "Currency": stock_info.get("currency", "N/A"),
-                    "QuoteType": stock_info.get("quoteType", "N/A"),
-                    "ShortName": stock_info.get("shortName", "N/A"),
-                    "Price": stock_info.get("regularMarketPrice", "N/A"),
-                    "52WeekHigh": stock_info.get("fiftyTwoWeekHigh", "N/A"),
-                    "52WeekLow": stock_info.get("fiftyTwoWeekLow", "N/A"),
-                    "DividendRate": stock_info.get("dividendRate", "N/A"),
-                    "DividendYield": stock_info.get("dividendYield", "N/A"),
-                    "PayoutRatio": stock_info.get("payoutRatio", "N/A"),
-                    "Beta": stock_info.get("beta", "N/A"),
-                    "PE": stock_info.get("trailingPE", "N/A"),
-                    "EPS": stock_info.get("trailingEps", "N/A"),
-                    "Revenue": stock_info.get("totalRevenue", "N/A"),
-                    "GrossProfit": stock_info.get("grossProfits", "N/A"),
-                    "FreeCashFlow": stock_info.get("freeCashflow", "N/A"),
-                }
-                stock_information.append(stock_data)
-            except Exception as e:
-                print(f"Error fetching stock information for {ticker}: {e}")
-                stock_information.append(
-                    {
-                        "Ticker": ticker,
-                        "Sector": "N/A",
-                        "Subsector": "N/A",
-                        "FullName": "N/A",
-                        "MarketCap": "N/A",
-                        "Country": "N/A",
-                        "Website": "N/A",
-                        "Description": "N/A",
-                        "CEO": "N/A",
-                        "Employees": "N/A",
-                        "City": "N/A",
-                        "State": "N/A",
-                        "Zip": "N/A",
-                        "Address": "N/A",
-                        "Phone": "N/A",
-                        "Exchange": "N/A",
-                        "Currency": "N/A",
-                        "QuoteType": "N/A",
-                        "ShortName": "N/A",
-                        "Price": "N/A",
-                        "52WeekHigh": "N/A",
-                        "52WeekLow": "N/A",
-                        "DividendRate": "N/A",
-                        "DividendYield": "N/A",
-                        "PayoutRatio": "N/A",
-                        "Beta": "N/A",
-                        "PE": "N/A",
-                        "EPS": "N/A",
-                        "Revenue": "N/A",
-                        "GrossProfit": "N/A",
-                        "FreeCashFlow": "N/A",
-                    }
-                )
-    return stock_information
+        for ticker in batch:
+            stock_info = yf.Ticker(ticker).info
+            stock_data = {
+                "Ticker": ticker,
+                "Sector": stock_info.get("sector", "N/A"),
+                "Subsector": stock_info.get("industry", "N/A"),
+                "FullName": stock_info.get("longName", "N/A"),
+                "MarketCap": stock_info.get("marketCap", "N/A"),
+                "Country": stock_info.get("country", "N/A"),
+                "Website": stock_info.get("website", "N/A"),
+                "Description": stock_info.get("longBusinessSummary", "N/A"),
+                "CEO": stock_info.get("ceo", "N/A"),
+                "Employees": stock_info.get("fullTimeEmployees", "N/A"),
+                "City": stock_info.get("city", "N/A"),
+                "State": stock_info.get("state", "N/A"),
+                "Zip": stock_info.get("zip", "N/A"),
+                "Address": stock_info.get("address1", "N/A"),
+                "Phone": stock_info.get("phone", "N/A"),
+                "Exchange": stock_info.get("exchange", "N/A"),
+                "Currency": stock_info.get("currency", "N/A"),
+                "QuoteType": stock_info.get("quoteType", "N/A"),
+                "ShortName": stock_info.get("shortName", "N/A"),
+                "Price": stock_info.get("regularMarketPrice", "N/A"),
+                "52WeekHigh": stock_info.get("fiftyTwoWeekHigh", "N/A"),
+                "52WeekLow": stock_info.get("fiftyTwoWeekLow", "N/A"),
+                "DividendRate": stock_info.get("dividendRate", "N/A"),
+                "DividendYield": stock_info.get("dividendYield", "N/A"),
+                "PayoutRatio": stock_info.get("payoutRatio", "N/A"),
+                "Beta": stock_info.get("beta", "N/A"),
+                "PE": stock_info.get("trailingPE", "N/A"),
+                "EPS": stock_info.get("trailingEps", "N/A"),
+                "Revenue": stock_info.get("totalRevenue", "N/A"),
+                "GrossProfit": stock_info.get("grossProfits", "N/A"),
+                "FreeCashFlow": stock_info.get("freeCashflow", "N/A"),
+            }
+            stock_information.append(stock_data)
+    return pd.DataFrame(stock_information)
 
 
-# Function to store stock information in SQLite
-def store_stock_information_in_sqlite(stock_information, db_path):
-    conn = sqlite3.connect(os.path.expanduser(db_path))
-    df = pd.DataFrame(stock_information)
-    df.to_sql("stock_information", conn, if_exists="replace", index=False)
-    conn.close()
-
-
-# Main execution
 def main():
-    db_dir = os.path.expanduser("~/personal_git/stock_price_predictor/db")
-    db_path = os.path.join(db_dir, "stock_data.db")
+    conn = sqlite3.connect("_stock_data.db")
+    tickers = fetch_sp500_tickers()
 
-    # Check if the database directory exists, create if not
-    if not os.path.exists(db_dir):
-        os.makedirs(db_dir)
+    quant_data = fetch_quant_data(tickers)
+    quant_data.to_sql("stock_data", conn, if_exists="replace", index=False)
 
-    # Check if the database file exists, create if not
-    if not os.path.exists(db_path):
-        try:
-            conn = sqlite3.connect(db_path)
-            conn.execute("""
-            CREATE TABLE IF NOT EXISTS stock_data (
-                Date TEXT,
-                Ticker TEXT,
-                Open REAL,
-                High REAL,
-                Low REAL,
-                Close REAL,
-                Adj_Close REAL,
-                Volume INTEGER
-            )
-            """)
-            conn.execute("""
-            CREATE TABLE IF NOT EXISTS stock_information (
-                Ticker TEXT,
-                Sector TEXT,
-                Subsector TEXT,
-                FullName TEXT,
-                MarketCap REAL,
-                Country TEXT,
-                Website TEXT,
-                Description TEXT,
-                CEO TEXT,
-                Employees INTEGER,
-                City TEXT,
-                State TEXT,
-                Zip TEXT,
-                Address TEXT,
-                Phone TEXT,
-                Exchange TEXT,
-                Currency TEXT,
-                QuoteType TEXT,
-                ShortName TEXT,
-                Price REAL,
-                "52WeekHigh" REAL,
-                "52WeekLow" REAL,
-                DividendRate REAL,
-                DividendYield REAL,
-                PayoutRatio REAL,
-                Beta REAL,
-                PE REAL,
-                EPS REAL,
-                Revenue REAL,
-                GrossProfit REAL,
-                FreeCashFlow REAL
-            )
-            """)
+    qual_data = fetch_qual_data(tickers)
+    qual_data.to_sql("stock_information", conn, if_exists="replace", index=False)
 
-            # Create the view if it doesn't exist
-            conn.execute("""
-            CREATE VIEW IF NOT EXISTS historicals_with_sector AS
-            SELECT
-                sd.Date,
-                sd.Ticker,
-                sd.Open,
-                sd.High,
-                sd.Low,
-                sd.Close,
-                sd.Volume,
-                si.Sector,
-                si.Subsector
-            FROM stock_data sd
-            JOIN stock_information si ON sd.Ticker = si.Ticker
-            """)
-            conn.close()
-        except sqlite3.Error as e:
-            print(f"SQLite error while creating tables/view: {e}")
-        except Exception as e:
-            print(f"Error while creating tables/view: {e}")
-
-    tickers = get_sp500_tickers()
-    batch_size = 503  # Set batch size as needed
-
-    print("Downloading stock data...")
-    all_data = download_stock_data(tickers, batch_size)
-
-    print("Storing stock data to SQLite database...")
-    store_stock_data_in_sqlite(all_data, db_path)
-
-    stock_information = get_stock_information_in_batches(tickers, batch_size)
-
-    print("Storing comprehensive stock information to SQLite database...")
-    store_stock_information_in_sqlite(stock_information, db_path)
-
-    print("Data download and storage complete.")
+    conn.close()
 
 
 if __name__ == "__main__":
